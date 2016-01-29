a7508b69319bed91508cb1721bc16b4839813cd3
Johannes Jendersie <jojendersie@web.de>
2016-01-30 00:04:27 +0100

    a WINDOW! (changed to SDL2)

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,37 @@
-extern crate piston;
-extern crate graphics;
-extern crate glutin_window;
-extern crate opengl_graphics;
+extern crate sdl2;
 
-use piston::window::WindowSettings;
-use piston::event_loop::*;
-use piston::input::*;
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{ GlGraphics, OpenGL };
+static mut running: bool = true;
 
-pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64   // Rotation for the square.
-}
-
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
-
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64,
-                      (args.height / 2) as f64);
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(GREEN, gl);
-
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
-
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
-        });
-    }
-
-    fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
-    }
+fn handle_event(event: sdl2::event::Event) {
+	use sdl2::event::Event;
+	use sdl2::keyboard::Keycode;
+	match event {
+		Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => unsafe {
+				running = false;
+			},
+			_ => {}
+	}
 }
 
 fn main() {
-    // Change this to OpenGL::V2_1 if not working.
-    let opengl = OpenGL::V3_2;
-
-    // Create an Glutin window.
-    let mut window: Window = WindowSettings::new(
-            "spinning-square",
-            [200, 200]
-        )
-        .opengl(opengl)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
-
-    // Create a new game and run it.
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        rotation: 0.0
-    };
-
-    let mut events = window.events();
-    while let Some(e) = events.next(&mut window) {
-        if let Some(r) = e.render_args() {
-            app.render(&r);
-        }
-
-        if let Some(u) = e.update_args() {
-            app.update(&u);
-        }
-    }
+	let sdl_context = sdl2::init().ok().expect("Failed to create context.");
+	let video_sys = sdl_context.video().ok().expect("Failed to create video context.");
+	let window = video_sys.window("Rituler", 1600, 900).build().ok().expect("Failed to create window.");
+	let mut renderer = window.renderer().build().ok().expect("Failed to create renderer.");
+	
+	// Resources TODO: move to structures
+	//let texture = renderer.
+	
+	while unsafe{running} {
+		let mut event_pump = sdl_context.event_pump().unwrap();
+		
+		// Handle all sdl events.
+		for event in event_pump.poll_iter() {
+			handle_event(event);
+		}
+		
+		renderer.clear();
+		//renderer.copy(&texture, None, Some(Rect::new_unwrap(100, 100, 220, 224)));
+		renderer.present();
+	}
 }
