6301e32e9d8d08c71a809bd643bf2d54e85cb9ce
Johannes Jendersie <jojendersie@web.de>
2016-01-30 00:54:40 +0100

    loading pngs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 extern crate sdl2;
+extern crate sdl2_image;
+
+//use sdl2_image::{self, LoadTexture, INIT_PNG, INIT_JPG};
+use sdl2_image::LoadTexture;
+use std::path::Path;
 
 static mut running: bool = true;
 
@@ -16,11 +21,12 @@ fn handle_event(event: sdl2::event::Event) {
 fn main() {
 	let sdl_context = sdl2::init().ok().expect("Failed to create context.");
 	let video_sys = sdl_context.video().ok().expect("Failed to create video context.");
+	let _image_context = sdl2_image::init(sdl2_image::INIT_PNG | sdl2_image::INIT_JPG);
 	let window = video_sys.window("Rituler", 1600, 900).build().ok().expect("Failed to create window.");
 	let mut renderer = window.renderer().build().ok().expect("Failed to create renderer.");
 	
 	// Resources TODO: move to structures
-	//let texture = renderer.
+	let texture = renderer.load_texture(&Path::new("img/mage.png")).unwrap();
 	
 	while unsafe{running} {
 		let mut event_pump = sdl_context.event_pump().unwrap();
@@ -32,6 +38,7 @@ fn main() {
 		
 		renderer.clear();
 		//renderer.copy(&texture, None, Some(Rect::new_unwrap(100, 100, 220, 224)));
+		renderer.copy(&texture, None, None);
 		renderer.present();
 	}
 }
