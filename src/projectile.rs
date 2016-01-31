1bb9c775c116545f9103f2ed97356107801d9d76
Thanduriel <rojendersie@alice.de>
2016-01-31 11:17:16 +0100

    + projectiles have a glittery tail

--- src/projectile.rs
+++ src/projectile.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
 
-use sdl2::rect::{Point};
+use sdl2::rect::{Point, Rect};
+use sdl2::pixels;
 
 use math;
 use drawable;
@@ -27,6 +28,7 @@ pub struct Projectile<'a> {
 	pub m_velocity : math::Vector,
 	pub m_is_finished : bool,
 	pub m_damage : f32,
+	pub m_life_time : i32,
 }
 
 impl <'a> drawable::Drawable for Projectile<'a>
@@ -34,6 +36,25 @@ impl <'a> drawable::Drawable for Projectile<'a>
 	fn draw(&self, _renderer : &mut sdl2::render::Renderer, _cam_pos : &Point) {
 		// Draw a life bar
 		
+		let offset = Point::new(self.m_velocity.x as i32, self.m_velocity.y as i32);
+		let baseLoc = Point::new((self.m_sprite.m_location.x as i32) - _cam_pos.x() - offset.x()*5, (self.m_sprite.m_location.y as i32) - _cam_pos.y() - offset.y()*5 );
+		
+		_renderer.set_draw_color(pixels::Color::RGB(230,140,20));
+		
+		let max = if self.m_life_time < 48 {
+			self.m_life_time / 2
+		} else{
+			24
+		};
+		for i in 0..max{
+			let randOff = i + 40;
+			let i_2 = randOff / 2; 
+			let point = Point::new(baseLoc.x() - i * offset.x() + math::get_rand(randOff as u32) - (i_2) - 2, baseLoc.y() - i * offset.y() + math::get_rand(randOff as u32) - i_2 - 2);
+		//	_renderer.draw_point();
+			_renderer.fill_rect(Rect::new(point.x(), point.y(), 4, 4).unwrap().unwrap());
+		}
+		
+		//draw the sprite last
 		(&self.m_sprite as &drawable::Drawable).draw(_renderer, _cam_pos);
 	}
 }
@@ -46,6 +67,7 @@ impl<'a> Projectile<'a> {
 			m_velocity: _vel,
 			m_is_finished : false,
 			m_damage : _damage,
+			m_life_time : 0,
 		}
 	}
 }
@@ -55,5 +77,6 @@ impl <'a> actor::Dynamic for Projectile<'a>
 	fn process(&mut self)
 	{
 		self.m_sprite.m_location = self.m_sprite.m_location + self.m_velocity;
+		self.m_life_time = self.m_life_time + 1;
 	}
 }
