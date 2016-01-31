0b4ffd1cbd04eb2dea885a14ad6dcdc066d3c731
Thanduriel <rojendersie@alice.de>
2016-01-31 16:12:31 +0100

    # controller is more general purpose

--- src/controller.rs
+++ src/controller.rs
@@ -6,18 +6,17 @@ use std::f32;
 
 pub struct Controller
 {
-//	pub m_actor : &'a actor::Actor<'a>,
-//	pub m_player : &'a player::Player<'a>,
 	pub m_speed : f32, // movement speed
+	pub m_prefered_dist : f32,
 }
 
 impl Controller{
-	pub fn think(&mut self, _actor: &mut actor::Actor, _player : & player::Player){
-		let mut dir = _actor.m_sprite.m_location - _player.m_actor.m_sprite.m_location;
+	pub fn think(&mut self, _actor: &mut actor::Actor, _target : & actor::Actor){
+		let mut dir = _actor.m_sprite.m_location - _target.m_sprite.m_location;
 		let len = dir.len();
 		dir.normalize();
 		
-		if len > 144.0 {
+		if len > self.m_prefered_dist {
 			_actor.m_sprite.m_location = _actor.m_sprite.m_location - dir * self.m_speed;
 		}
 		_actor.m_sprite.m_angle = f32::atan2(dir.y, dir.x) * 180.0 / f32::consts::PI + 90.0;
